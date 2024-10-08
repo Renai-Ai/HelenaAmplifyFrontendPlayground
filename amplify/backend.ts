220acb7563ff909d39bd7e87820bb960863c2506
njsaugat <njsaugat@gmail.com>
2024-10-08 21:03:35 +0545

    add custom function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,18 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
-
-defineBackend({
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { HelloWorldLambdaStack } from "./custom-functions/resources";
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+const backend = defineBackend({
   auth,
   data,
 });
+
+// Add the HelloWorld custom Lambda stack to the backend
+new HelloWorldLambdaStack(
+  backend.createStack("HelloWorldLambdaStack"),
+  "helloWorldLambdaResource",
+  {}
+);
