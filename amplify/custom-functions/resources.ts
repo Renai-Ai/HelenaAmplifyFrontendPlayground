60f4cb05e6949976d0dfa8ec323c3dda2966f0e2
Prakash Manandhar <jsaaymi@gmail.com>
2024-10-09 10:55:44 -0400

    Add function name change based on environment variable LAMBDA_PREFIX

--- amplify/custom-functions/resources.ts
+++ amplify/custom-functions/resources.ts
@@ -2,17 +2,19 @@ import { CfnOutput, Stack, StackProps, Duration } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
+import { defineFunction } from '@aws-amplify/backend';
+
+const fnName = process.env.LAMBDA_PREFIX + "HelloWorldFunction";
 
 export class HelenaHelloWorldLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
     // Define the Lambda function
-    const helloWorldFunction = new lambda.Function(this, "HelenaHelloWorldFunction", {
+    const helloWorldFunction = new lambda.Function(this, fnName, {
       runtime: lambda.Runtime.PYTHON_3_9, // Specify the runtime
       handler: "index.handler", // Specify the handler function
       code: lambda.Code.fromAsset("./amplify/custom-functions/helloworld"),
-      functionName: "HelenaHelloWorldFunction",
+      functionName: fnName,
       description: "This is my custom Lambda function created using CDK",
       timeout: Duration.seconds(30),
       memorySize: 128,
@@ -21,9 +23,9 @@ export class HelenaHelloWorldLambdaStack extends Stack {
       },
     });
     // Output the Lambda function ARN
-    new CfnOutput(this, "HelenaHellowWorldFunctionArn", {
+    new CfnOutput(this, fnName + "Arn", {
       value: helloWorldFunction.functionArn,
-      exportName: "HelenaHelloWorldFunctionArn",
+      exportName: fnName + "Arn",
     });
   }
 }
