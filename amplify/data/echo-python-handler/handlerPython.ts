4ed9646af262b4cf2963964242aa71da19acb86e
Prakash Manandhar <jsaaymi@gmail.com>
2025-04-06 10:49:47 -0400

    Updated Lambda URL

--- amplify/data/echo-python-handler/handlerPython.ts
+++ amplify/data/echo-python-handler/handlerPython.ts
@@ -3,7 +3,7 @@ import got from 'got';
 
 export const handler: Schema["echoPython"]["functionHandler"] = async (event, context) => {
   const start = performance.now();
-  var response = await got.post(`https://iiygu53tm6xjmdo7kemqoaytey0xfido.lambda-url.us-east-2.on.aws/`,{
+  var response = await got.post(`https://4kp73orvs2hov7yropu6wyypp40mbqlm.lambda-url.us-east-1.on.aws/`,{
     json: {
       message: event.arguments.content
     }
@@ -11,7 +11,7 @@ export const handler: Schema["echoPython"]["functionHandler"] = async (event, co
   var result = JSON.stringify(response);
   var JSONresult = JSON.parse(result);
   return {
-    content: `Echoing Python content [changed 2024-Nov-03]: ${JSONresult.message}`,
+    content: `Echoing Python content [changed 2025-Apr-06]: ${JSONresult.message}`,
     executionDuration: performance.now() - start
   };
 };
